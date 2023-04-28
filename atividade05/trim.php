4b9323909b99c07fd6af22a89150e97b2c6a28b6
JeanMaxSKrebs <jeanmaxskrebs@gmail.com>
2023-04-28 14:56:51 -0300

    trim

---
+++ atividade05/trim.php
@@ -0,0 +1,19 @@
+<?php
+echo "<h2>Usando a função trim()</h2>";
+// usando a função trim
+echo trim("  stringhere  ");
+echo '<br/>';
+
+echo "<h2>Usando a função ltrim()</h2>";
+// usando a função ltrim
+echo ltrim(" stringhere ");
+echo '<br/>';
+
+
+echo "<h2>Usando a função rtrim()</h2>";
+// usando a função rtrim
+echo rtrim(" stringhere  ");
+echo '<br/>';
+
+
+// https://squids.com.br/editorPHP/index.php?id=11
